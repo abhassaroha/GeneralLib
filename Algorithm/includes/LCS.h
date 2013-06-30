a87ea88abcd9de4b58bba5e4c3a737db8e889c89
abhassaroha <abhassaroha@gmail.com>
2013-06-30 17:04:05 +0530

    boiler plate code for LCS problem

---
+++ Algorithm/includes/LCS.h
@@ -0,0 +1,24 @@
+#ifndef __LCS__H
+#define __LCS__H
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class LCS {
+	private:
+		char* mFile1;
+		char* mFile2;
+	public:
+		LCS(char* file1, char* file2)
+			:mFile1(file1), mFile2(file2) {}
+		// Simple dynamic programming solution
+		void generateDiffDP();
+		// James W. Hunt and M. Douglas McIlroy (June 1976). 
+		// "An Algorithm for Differential File Comparison". 
+		// Computing Science Technical Report, Bell Laboratories 41.
+		void generateDiffHM();
+};
+
+#endif
---
+++ Algorithm/src/LCS.cpp
@@ -0,0 +1,46 @@
+#include "LCS.h"
+
+void printSyntax() {
+	cout<<"Usage:"<<endl;
+	cout<<"diff -d/-h file1 file2"<<endl;
+}
+
+void LCS::generateDiffDP() {
+}
+
+void LCS::generateDiffHM() {
+}
+
+int main(int argc, char** argv) {
+	LCS* instance;
+	if (argc < 2) {
+		cout<<"No argument supplied"<<endl;
+		printSyntax();
+		exit(EXIT_FAILURE);
+	}
+	else if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h")) {
+		printSyntax();
+		exit(EXIT_SUCCESS);
+	}
+
+	if (argc < 4) {
+		cout<<"Wrong usage"<<endl;
+		printSyntax();
+		exit(EXIT_FAILURE);
+	}
+
+	if (strcmp(argv[1], "-d") == 0) {
+		instance = new LCS(argv[2], argv[3]);
+		instance->generateDiffDP();
+	}
+	else if (strcmp(argv[1], "-m") == 0) {
+		instance = new LCS(argv[2], argv[3]);
+		instance->generateDiffHM();
+	}
+	else {
+		cout<<"Wrong usage"<<endl;
+		printSyntax();
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
